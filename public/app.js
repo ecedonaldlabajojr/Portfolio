3da8ce7720c3f139354d242cb30ca6d88a1034a5
Donald P. Labajo Jr <ecedonaldlabajojr@gmail.com>
2020-12-14 23:21:15 +0800

    only added process.env.PORT for heroku push

--- public/app.js
+++ public/app.js
@@ -54,7 +54,7 @@ app.post("/", function (req, res) {
     // res.send("RECEIVED!");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server started on port: 3000");
 });
 
